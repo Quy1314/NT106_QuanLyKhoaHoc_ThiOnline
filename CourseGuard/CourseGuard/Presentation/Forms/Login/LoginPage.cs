2e6d40c224a6736129700026021d2bc4c1736073
NhutQuoc <nhutquoc2910@gmail.com>
2026-04-06 01:10:58 +0700

    Update Theme and RedirectForm and LoginPage

--- CourseGuard/CourseGuard/Presentation/Forms/Login/LoginPage.cs
+++ CourseGuard/CourseGuard/Presentation/Forms/Login/LoginPage.cs
@@ -28,7 +28,8 @@ namespace CourseGuard.Presentation.Forms.Login
             // Form Background
             this.BackColor = Color.FromArgb(242, 244, 248); // Light Gray
             this.FormBorderStyle = FormBorderStyle.Sizable; // User requested Sizable
-
+            this.AcceptButton = btnLogin;
+            
             // Panel Style
             LoginPanel.BackColor = Color.White;
             LoginPanel.BorderStyle = BorderStyle.None;
